abdf0393ed9389603ade8ccb70e66bbba55a47a6
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-04-25 08:54:30 +0900

    added header and album list conponents

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,15 @@
 // Index.ios.js
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
- <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
